dfeebce9e200d7faf40f09f8b8e43ad438da5fd4
Toby Dawson <tobydawson1@gmail.com>
2020-02-19 16:06:57 +0000

    added minimum temperature

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -11,11 +11,16 @@ describe('Thermostat', function() {
 
     it('increases in temp', function() {
         
-        expect(thermostat.increase()).toEqual(21);
+        expect(thermostat.increase(1)).toEqual(21);
     });
  
     it('decreases in temp', function() {
-        expect(thermostat.decrease()).toEqual(19);
+        expect(thermostat.decrease(1)).toEqual(19);
+    });
+
+    it('throws error at minimum temperature', function() {
+        thermostat.decrease(10)
+        expect(function(){thermostat.decrease();}).toThrow(new Error("Temperature is too low"));
     });
 });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,17 +1,21 @@
 function Thermostat() {
     this.temp = 20;
+    this.minimumTemperature = 10;
 }
 
 Thermostat.prototype.temperature = function() {
     return this.temp
 }
 
-Thermostat.prototype.increase = function() {
-    return this.temp += 1
+Thermostat.prototype.increase = function(increment) {
+    return this.temp += increment
     
 }
 
-Thermostat.prototype.decrease = function() {
-    return this.temp -= 1
+Thermostat.prototype.decrease = function(increment) {
+    if (this.temp <= this.minimumTemperature)
+        throw new Error("Temperature is too low")
+    else
+    return this.temp -= increment
 }
 
